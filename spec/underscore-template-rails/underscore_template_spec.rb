64bf1e8b52a517b2e5762de4a5e6ad4f986cc466
andigr <andigr07@gmail.com>
2014-04-02 18:10:33 +0300

    Fixed specs

--- spec/underscore-template-rails/underscore_template_spec.rb
+++ spec/underscore-template-rails/underscore_template_spec.rb
@@ -6,14 +6,14 @@ describe UnderscoreTemplateRails::UnderscoreTemplate do
   it 'compiles templates with the .utmp extension' do
     Rails.configuration.underscore_templates.variable = 'templates'
     template = Rails.application.assets['templates/test']
-    template.to_s.should == "window.templates = window.templates || []; templates[\"test\"] = _.template(\"<div class=\\\"test\\\">\\n  <%= test %>\\n<\\/div>\\n\");"
+    template.to_s.should == "window.templates = window.templates || {}; templates[\"test\"] = _.template(\"<div class=\\\"test\\\">\\n  <%= test %>\\n<\\/div>\\n\");"
   end
 
   context 'when config.template_variable is set' do
     it 'sets the variable from the config' do
       Rails.configuration.underscore_templates.variable = 'argon'
       template = Rails.application.assets['templates/test']
-      template.to_s.should == "window.argon = window.argon || []; argon[\"test\"] = _.template(\"<div class=\\\"test\\\">\\n  <%= test %>\\n<\\/div>\\n\");"
+      template.to_s.should == "window.argon = window.argon || {}; argon[\"test\"] = _.template(\"<div class=\\\"test\\\">\\n  <%= test %>\\n<\\/div>\\n\");"
     end
   end
 
